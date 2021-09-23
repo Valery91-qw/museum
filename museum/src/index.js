4ff0a085e42b436815548fa58906a78ab047fe36
Valery91-qw <vakriv91@gmail.com>
2021-09-23 14:36:12 +0300

    feat: add form to tickets-section

--- museum/src/index.js
+++ museum/src/index.js
@@ -1,2 +1,10 @@
 import "./style.scss"
 import "./assets/svg/louvre-icon.svg"
+
+
+
+const formBuyTickets = document.getElementById('buy-tickets');
+
+formBuyTickets.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
