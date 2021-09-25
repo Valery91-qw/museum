f71fa8b8f2cafb93c54ecae106f4d7b7dd19a3a6
Valery91-qw <vakriv91@gmail.com>
2021-09-25 13:11:31 +0300

    fix: fix markers mapping

--- museum/src/js/mapbox.js
+++ museum/src/js/mapbox.js
@@ -9,24 +9,17 @@ const map = new mapboxgl.Map({
     center: [2.3364, 48.86091],
     zoom: 16,
 });
+
 new mapboxgl.Marker({color: 'black'})
     .setLngLat([2.3364, 48.86091])
     .addTo(map);
 
-new mapboxgl.Marker({color: 'gray'})
-    .setLngLat([2.3333, 48.8602])
-    .addTo(map);
+const mapMarkers = [[2.3333, 48.8602], [2.3397, 48.8607], [2.3330, 48.8619], [2.3365, 48.8625]];
 
-new mapboxgl.Marker({color: 'gray'})
-    .setLngLat([2.3397, 48.8607])
-    .addTo(map);
-
-new mapboxgl.Marker({color: 'gray'})
-    .setLngLat([2.3330, 48.8619])
-    .addTo(map);
-
-new mapboxgl.Marker({color: 'gray'})
-    .setLngLat([2.3365, 48.8625])
-    .addTo(map);
+mapMarkers.forEach(geoData => {
+    new mapboxgl.Marker({color: 'gray'})
+        .setLngLat(geoData)
+        .addTo(map);
+});
 
 map.addControl(new mapboxgl.NavigationControl());
