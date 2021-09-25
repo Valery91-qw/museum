cb4a50f44d37bcceacb405778925a4b7c8cbe3c0
Valery91-qw <vakriv91@gmail.com>
2021-09-25 13:00:55 +0300

    feat: finish contacts-section

--- museum/src/js/mapbox.js
+++ museum/src/js/mapbox.js
@@ -5,7 +5,28 @@ const mapElement = document.getElementById('map')
 mapboxgl.accessToken = apiKey;
 const map = new mapboxgl.Map({
     container: mapElement,
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: [2.3364, 48.86091],
     zoom: 16,
 });
+new mapboxgl.Marker({color: 'black'})
+    .setLngLat([2.3364, 48.86091])
+    .addTo(map);
+
+new mapboxgl.Marker({color: 'gray'})
+    .setLngLat([2.3333, 48.8602])
+    .addTo(map);
+
+new mapboxgl.Marker({color: 'gray'})
+    .setLngLat([2.3397, 48.8607])
+    .addTo(map);
+
+new mapboxgl.Marker({color: 'gray'})
+    .setLngLat([2.3330, 48.8619])
+    .addTo(map);
+
+new mapboxgl.Marker({color: 'gray'})
+    .setLngLat([2.3365, 48.8625])
+    .addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
