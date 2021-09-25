cc8126d15fd7851aa409dd02c6459a07b6fbbc57
Valery91-qw <vakriv91@gmail.com>
2021-09-25 19:59:58 +0300

    feat: add frame to welcome-section

--- museum/webpack.config.js
+++ museum/webpack.config.js
@@ -13,6 +13,7 @@ const devServer = (isDev) => !isDev ? {} : {
 
 let allHtmlFileName = [
     'index',
+    'discoverTheLouvre',
     'tourRoyalPalace',
     'tourDenonWing',
     'tourColonnadePerrault',
