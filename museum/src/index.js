0bef96656c1d55534026ac8df2e7e0899d42081b
Valery91-qw <vakriv91@gmail.com>
2021-09-27 13:45:51 +0300

    feat: finish modal-window and open-close logic

--- museum/src/index.js
+++ museum/src/index.js
@@ -8,4 +8,32 @@ const formBuyTickets = document.getElementById('buy-tickets');
 
 formBuyTickets.addEventListener('submit', (event) => {
     event.preventDefault();
+    const modal = document.getElementById('modal-window')
+    const overlay = document.getElementById('overlay')
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+    overlay.style.opacity = "1";
+    overlay.style.visibility = "visible";
 });
+
+const modalClose = document.getElementById('modal-close')
+
+modalClose.addEventListener('click', (event) => {
+    const modal = document.getElementById('modal-window')
+    const overlay = document.getElementById('overlay')
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+    overlay.style.opacity = "0";
+    overlay.style.visibility = "hidden";
+})
+
+const overlay = document.getElementById('overlay')
+
+overlay.addEventListener('click', () => {
+    const modal = document.getElementById('modal-window')
+    const overlay = document.getElementById('overlay')
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+    overlay.style.opacity = "0";
+    overlay.style.visibility = "hidden";
+})
