ffdb874b11e1c7a3da50ff7e9b71259a40b626cd
Valery91-qw <vakriv91@gmail.com>
2021-09-30 18:56:31 +0300

    feat: create slider in welcome-sections and refactor

--- museum/src/js/welcome-carousel.js
+++ museum/src/js/welcome-carousel.js
@@ -1,10 +1,53 @@
 
+let carouselItems = document.querySelectorAll('.welcome-carousel-item');
+const display = document.querySelector('.welcome-carousel-controls-display');
 
-const ul = document.getElementById('elements')
-const current = document.getElementById('current')
+let currentItem = 0;
+let isEnabled = true;
 
-ul.addEventListener('click', (event) => {
-  if(event.target.tagName === 'LI'){
-      current.innerText = event.target.innerText;
-  }
+function changeItem(item) {
+    currentItem =  (item + carouselItems.length) % carouselItems.length
+    display.innerText = `0${currentItem + 1} | 0${carouselItems.length}`
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    carouselItems[currentItem].classList.add(direction)
+    carouselItems[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('active', direction)
+    })
+}
+function showItem(direction) {
+    carouselItems[currentItem].classList.add('next', direction)
+    carouselItems[currentItem].addEventListener('animationend', function () {
+        this.classList.remove('next', direction)
+        this.classList.add('active')
+        isEnabled = true
+    })
+}
+
+
+
+function previousItem(item) {
+    hideItem('to-right')
+    changeItem(item - 1)
+    showItem('from-right')
+}
+
+function nextItem(item) {
+    hideItem('to-left')
+    changeItem(item + 1)
+    showItem('from-left')
+}
+
+document.querySelector('.control.prev').addEventListener('click', function(){
+    if(isEnabled) {
+        previousItem(currentItem)
+    }
+})
+
+document.querySelector('.control.next').addEventListener('click', function(){
+    if(isEnabled) {
+        nextItem(currentItem)
+    }
 })
