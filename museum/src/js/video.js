a2366af0bffb994f4d4445f67560926333176576
Valery91-qw <vakriv91@gmail.com>
2021-10-03 19:44:42 +0300

    feat: layout inputs in video sections

--- museum/src/js/video.js
+++ museum/src/js/video.js
@@ -1,3 +1,14 @@
 let videoPlayer = document.getElementById('video-player');
 let inputs = document.querySelectorAll('input[type=range]');
 
+inputs.forEach(element => {
+    element.addEventListener('input', (event) => {
+        let target = event.target
+
+        let min = target.min
+        let max = target.max
+        let value = target.value
+
+        target.style.backgroundImage = `linear-gradient(to right, #710707 ${value}%, #C4C4C4 0%)`
+    })
+})
