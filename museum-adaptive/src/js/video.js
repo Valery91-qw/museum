f2cccf0f5fb7d40af510a0a2af950b213573a23b
Valery91-qw <vakriv91@gmail.com>
2021-10-04 14:41:30 +0300

    fix: rename variables

--- museum-adaptive/src/js/video.js
+++ museum-adaptive/src/js/video.js
@@ -1,15 +1,18 @@
 import stop from '../assets/img/video/pause.svg'
 import play from '../assets/img/video/control-play.svg'
 
-let videoPlayer = document.getElementById('video-player');
-let inputs = document.querySelectorAll('input[type=range]');
-let playButton = document.querySelector('.video-section-player-play-button')
-let controlButton = document.getElementById('video-control-play')
-let image = document.getElementById('play-image')
+let videoTrack = document.getElementById('video-player');
+
+let commonPlayButton = document.querySelector('.video-section-player-play-button')
+let videoControlButton = document.getElementById('video-control-play')
+let volumeControlButton = document.getElementById('volume-control-button')
+let videoPlayImage = document.getElementById('play-image')
+// inputs
+let inputsRange = document.querySelectorAll('input[type=range]');
 let volumeControl = document.getElementById('audio-track')
 let playControl = document.getElementById('video-track')
 
-inputs.forEach(element => {
+inputsRange.forEach(element => {
     element.addEventListener('input', (event) => {
         let target = event.target
 
@@ -18,17 +21,18 @@ inputs.forEach(element => {
         target.style.backgroundImage = `linear-gradient(to right, #710707 ${value}%, #C4C4C4 0%)`
     })
 })
+
 volumeControl.addEventListener('input', () => {
-    videoPlayer.volume = volumeControl.value / 100
+    videoTrack.volume = volumeControl.value / 100
 }, false)
+
 playControl.addEventListener('input', () => {
-    let duration = videoPlayer.duration
-    let current = (duration / 100) * playControl.value
-    videoPlayer.currentTime = current
+    let duration = videoTrack.duration
+    videoTrack.currentTime = (duration / 100) * playControl.value
 })
 
-videoPlayer.addEventListener('timeupdate', () => {
-    let value = (videoPlayer.currentTime / videoPlayer.duration) * 100
+videoTrack.addEventListener('timeupdate', () => {
+    let value = (videoTrack.currentTime / videoTrack.duration) * 100
     playControl.value = value
     playControl.style.backgroundImage = `linear-gradient(to right, #710707 ${value}%, #C4C4C4 0%)`
 })
@@ -37,20 +41,20 @@ const isPlaying = () => {
     let button = document.getElementById('play-button');
 
     if (button.classList.contains('hide')) {
-        videoPlayer.pause()
+        videoTrack.pause()
         button.classList.remove('hide')
-        image.src = play
+        videoPlayImage.src = play
     } else {
-        videoPlayer.play()
+        videoTrack.play()
         button.classList.add('hide')
-        image.src = stop
+        videoPlayImage.src = stop
     }
 }
 
-playButton.addEventListener('click', () => {
+commonPlayButton.addEventListener('click', () => {
     isPlaying()
 })
 
-controlButton.addEventListener('click', (event) => {
+videoControlButton.addEventListener('click', (event) => {
     isPlaying()
 })
