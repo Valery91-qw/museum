640c7b765abf4321c7635120cde3ef5e5cf057ff
Valery91-qw <vakriv91@gmail.com>
2021-10-07 16:45:25 +0300

    feat: bind the common fields in tickets-section and fields in form

--- museum-dom/src/js/tickets.js
+++ museum-dom/src/js/tickets.js
@@ -6,6 +6,12 @@ let totalCoast = document.getElementById('total-amount')
 let ticketTypeField = document.getElementById('modal-type-tickets')
 let ticketsType = document.getElementById('modal-type')
 let basicTicketCoast = document.getElementById('modal-basic-coast')
+let basicTicketsAmount = document.getElementById('modal-basic-amount')
+let basicTicketsTotalCoast = document.getElementById('modal-basic-total-coast')
+let seniorTicketCoast = document.getElementById('modal-senior-coast')
+let seniorTicketsAmount = document.getElementById('modal-senior-amount')
+let seniorTicketsTotalCoast = document.getElementById('modal-senior-total-coast')
+let modalTotalCoast = document.getElementById('modal-total-coast')
 
 let coast = 20
 
@@ -16,18 +22,29 @@ let box = document.querySelectorAll('.tickets-container-buy-form-tickets-amount-
 box.forEach(el => {
     el.addEventListener('click', () => {
         totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+        modalTotalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+
+        basicTicketsAmount.innerText = basicAmount.value
+        seniorTicketsAmount.innerText = seniorAmount.value
+
+        basicTicketsTotalCoast.innerText = String(basicAmount.value * coast)
+        seniorTicketsTotalCoast.innerText = String(seniorAmount.value * (coast / 2))
     })
 })
 
 document.getElementById('tickets-type').addEventListener('change', (event) => {
     coast = event.target.value
     totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+    modalTotalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
 
+    basicTicketsTotalCoast.innerText = String(basicAmount.value * coast)
+    seniorTicketsTotalCoast.innerText = String(seniorAmount.value * (coast / 2))
     ticketTypeField.innerText = event.target.dataset.name
 
     ticketsType.value = event.target.dataset.name
 
     basicTicketCoast.innerText = coast
+    seniorTicketCoast.innerText = String(coast / 2)
 })
 
 
