6f3b8b05d47f306f858c7e2cb114e71ba0af8321
Valery91-qw <vakriv91@gmail.com>
2021-10-07 16:15:38 +0300

    feat: bind the common fields in tickets-section

--- museum-dom/src/js/tickets.js
+++ museum-dom/src/js/tickets.js
@@ -1,18 +1,34 @@
+//common menu
 let basicAmount = document.getElementById('basic');
 let seniorAmount = document.getElementById('senior')
-let totalAmount = document.getElementById('total-amount')
+let totalCoast = document.getElementById('total-amount')
+//modal window
+let ticketTypeField = document.getElementById('modal-type-tickets')
+let ticketsType = document.getElementById('modal-type')
+let basicTicketCoast = document.getElementById('modal-basic-coast')
 
-totalAmount.innerText = ` ${(basicAmount.value * 10) + (seniorAmount.value * 5)}`
+let coast = 20
+
+totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
 
 let box = document.querySelectorAll('.tickets-container-buy-form-tickets-amount-quantity-box')
 
 box.forEach(el => {
     el.addEventListener('click', () => {
-        totalAmount.innerText = ` ${(basicAmount.value * 10) + (seniorAmount.value * 5)}`
-
+        totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
     })
 })
 
+document.getElementById('tickets-type').addEventListener('change', (event) => {
+    coast = event.target.value
+    totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+
+    ticketTypeField.innerText = event.target.dataset.name
+
+    ticketsType.value = event.target.dataset.name
+
+    basicTicketCoast.innerText = coast
+})
 
 
 
