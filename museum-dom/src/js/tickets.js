375fb3b76ad2cb8df7f2b702d659307a1fad5482
Valery91-qw <vakriv91@gmail.com>
2021-10-07 18:57:42 +0300

    feat: continue to add actions in modal window

--- museum-dom/src/js/tickets.js
+++ museum-dom/src/js/tickets.js
@@ -1,52 +1,75 @@
 //common menu
-let basicAmount = document.getElementById('basic');
+let basicAmount = document.getElementById('basic')
 let seniorAmount = document.getElementById('senior')
-let totalCoast = document.getElementById('total-amount')
+let commonTicketsType = document.getElementById('tickets-type')
+let totalCoastTextElements = document.querySelectorAll('.total-tickets-coast')
 //modal window
-let ticketTypeField = document.getElementById('modal-type-tickets')
+let ticketTypeText = document.getElementById('modal-tickets-type')
 let ticketsType = document.getElementById('modal-type')
-let basicTicketCoast = document.getElementById('modal-basic-coast')
-let basicTicketsAmount = document.getElementById('modal-basic-amount')
-let basicTicketsTotalCoast = document.getElementById('modal-basic-total-coast')
-let seniorTicketCoast = document.getElementById('modal-senior-coast')
-let seniorTicketsAmount = document.getElementById('modal-senior-amount')
-let seniorTicketsTotalCoast = document.getElementById('modal-senior-total-coast')
-let modalTotalCoast = document.getElementById('modal-total-coast')
+let basicTicketCoastText = document.querySelectorAll('.modal-basic-coast')
+let basicTicketsAmountText = document.getElementById('modal-basic-amount')
+let basicTicketsTotalCoastText = document.getElementById('modal-basic-total-coast')
+let seniorTicketCoastText = document.querySelectorAll('.modal-senior-coast')
+let seniorTicketsAmountText = document.getElementById('modal-senior-amount')
+let seniorTicketsTotalCoastText = document.getElementById('modal-senior-total-coast')
 
 let coast = 20
 
-totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+totalCoastTextElements.forEach(el => {
+    el.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+})
 
 let box = document.querySelectorAll('.tickets-container-buy-form-tickets-amount-quantity-box')
 
 box.forEach(el => {
+
     el.addEventListener('click', () => {
-        totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
-        modalTotalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
 
-        basicTicketsAmount.innerText = basicAmount.value
-        seniorTicketsAmount.innerText = seniorAmount.value
+        totalCoastTextElements.forEach(el => {
+            el.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+        })
 
-        basicTicketsTotalCoast.innerText = String(basicAmount.value * coast)
-        seniorTicketsTotalCoast.innerText = String(seniorAmount.value * (coast / 2))
+        basicTicketsAmountText.innerText = basicAmount.value
+        seniorTicketsAmountText.innerText = seniorAmount.value
+
+        basicTicketsTotalCoastText.innerText = String(basicAmount.value * coast)
+        seniorTicketsTotalCoastText.innerText = String(seniorAmount.value * (coast / 2))
     })
 })
 
-document.getElementById('tickets-type').addEventListener('change', (event) => {
+commonTicketsType.addEventListener('change', (event) => {
     coast = event.target.value
-    totalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
-    modalTotalCoast.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
 
-    basicTicketsTotalCoast.innerText = String(basicAmount.value * coast)
-    seniorTicketsTotalCoast.innerText = String(seniorAmount.value * (coast / 2))
-    ticketTypeField.innerText = event.target.dataset.name
+    totalCoastTextElements.forEach(el => {
+        el.innerText = ` ${(basicAmount.value * coast) + (seniorAmount.value * (coast / 2))}`
+    })
+
+    basicTicketsTotalCoastText.innerText = String(basicAmount.value * coast)
+    seniorTicketsTotalCoastText.innerText = String(seniorAmount.value * (coast / 2))
+    ticketTypeText.innerText = event.target.dataset.name
 
     ticketsType.value = event.target.dataset.name
 
-    basicTicketCoast.innerText = coast
-    seniorTicketCoast.innerText = String(coast / 2)
+    basicTicketCoastText.forEach(el => {
+        el.innerText = coast
+    })
+    seniorTicketCoastText.forEach(el => {
+        el.innerText = String(coast / 2)
+    })
 })
 
+ticketsType.addEventListener('change', function (event){
+    ticketTypeText.innerText = event.target.value
+
+    coast = this.selectedOptions[0].getAttribute('data-coast')
+
+    basicTicketCoastText.forEach(el => {
+        el.innerText = coast
+    })
+    seniorTicketCoastText.forEach(el => {
+        el.innerText = String(coast / 2)
+    })
+})
 
 
 
